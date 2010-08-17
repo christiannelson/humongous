61e19ab52b96e4e9fc00cc72685de86e6141bf3b
Christian Nelson <christian@carbonfive.com>
2010-08-16 22:18:03 -0700

    Uno

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Humongous::Application.config.secret_token = '827c9dda4483ee1a9fe9e96d5ecea59efa4bd4555258231480e271885e7c5720271e875e532169b52af943e1547f25d32f114b8e59f63ec3a0a555ebd0751f29'
