03035bc1340fa93ae684c46b1f63bc901cb31bb9
Christian Nelson <christian@carbonfive.com>
2010-08-22 22:42:48 -0700

    Removed scaffold cruft.

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,83 +1,46 @@
 class InspectionsController < ApplicationController
-  # GET /inspections
-  # GET /inspections.xml
   def index
     @inspections = Inspection.asc(:finished_at).all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @inspections }
-    end
+    #render :text => "index page: #{@inspections.collect { |i| i.total_observables }.sum} total observables for #{@inspections.length} inspections"
   end
 
-  # GET /inspections/1
-  # GET /inspections/1.xml
   def show
     @inspection = Inspection.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @inspection }
-    end
+    #render :text => "show page for #{@inspection.name}"
   end
 
-  # GET /inspections/new
-  # GET /inspections/new.xml
   def new
     @inspection = Inspection.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @inspection }
-    end
   end
 
-  # GET /inspections/1/edit
   def edit
     @inspection = Inspection.find(params[:id])
   end
 
-  # POST /inspections
-  # POST /inspections.xml
   def create
     @inspection = Inspection.new(params[:inspection])
 
-    respond_to do |format|
-      if @inspection.save
-        format.html { redirect_to(@inspection, :notice => 'Inspection was successfully created.') }
-        format.xml  { render :xml => @inspection, :status => :created, :location => @inspection }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @inspection.errors, :status => :unprocessable_entity }
-      end
+    if @inspection.save
+      redirect_to(@inspection, :notice => 'Inspection was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /inspections/1
-  # PUT /inspections/1.xml
   def update
     @inspection = Inspection.find(params[:id])
 
-    respond_to do |format|
-      if @inspection.update_attributes(params[:inspection])
-        format.html { redirect_to(@inspection, :notice => 'Inspection was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @inspection.errors, :status => :unprocessable_entity }
-      end
+    if @inspection.update_attributes(params[:inspection])
+      redirect_to(@inspection, :notice => 'Inspection was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /inspections/1
-  # DELETE /inspections/1.xml
   def destroy
     @inspection = Inspection.find(params[:id])
     @inspection.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(inspections_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(inspections_url, :notice => 'Inspection was successfully deleted.')
   end
 end
