7d70ee026befbee4e00980a414921758fcda8731
Christian Nelson <christian@carbonfive.com>
2010-10-27 17:26:47 -0700

    More verbose debugging so it's easy to see what's going on with mongoid.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,8 @@ Humongous::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  config.log_level = :debug
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 end
