7f2a21a161afc6deb9bdd8fd80954f6a0b1b8c3a
Christian Nelson <christian@carbonfive.com>
2010-10-30 20:15:52 -0700

    Increase the backlog and enabled preloading.

--- etc/unicorn.production.conf.rb
+++ etc/unicorn.production.conf.rb
@@ -18,7 +18,7 @@ working_directory "/home/deploy/humongous/current"
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/.sock", :backlog => 64
+listen "/tmp/.sock", :backlog => 1024
 #listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
@@ -35,7 +35,8 @@ stdout_path "/home/deploy/humongous/shared/log/unicorn.stdout.log"
 
 # combine REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-# preload_app true
+preload_app true
+
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
 
